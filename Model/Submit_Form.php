d51036531e2a4126dde0beffe16fee6c201ba78e
Houley <ethouley@gmail.com>
2018-05-20 09:59:47 +1000

    Submit_Form.php Now submits correctly instead of saying "Fatal error: Uncaught PDOException: SQLSTATE[23000]: Integrity constraint violation: 1452 Cannot add or update a child row: a foreign key constraint fails"

--- Model/Submit_Form.php
+++ Model/Submit_Form.php
@@ -2,15 +2,18 @@
   session_start();
   include '../Model/_Connection.php';
   require_once '../Model/Functions.php';
-  $UserID = filter_var(clean($_POST['UserID']), FILTER_SANITIZE_STRING);
+  $UserID = filter_var(clean($_SESSION['UserID']), FILTER_SANITIZE_STRING);
   $Room = filter_var(clean($_POST['Room']), FILTER_SANITIZE_STRING);
   $Broken_Mouse = filter_var(clean($_POST['Broken_Mouse']), FILTER_SANITIZE_STRING);
   $Broken_Keyboard = filter_var(clean($_POST['Broken_Keyboard']), FILTER_SANITIZE_STRING);
   $Broken_Screen = filter_var(clean($_POST['Broken_Screen']), FILTER_SANITIZE_STRING);
   $ExtraInfo = filter_var(clean($_POST['ExtraInfo']), FILTER_SANITIZE_STRING);
-  $JobRequest_sql = "INSERT INTO job (JobID, DateLogged, UserID, RoomID, AssetID, Broken_Mouse, Broken_Keyboard, Broken_Screen, ExtraInfo, JobStatusID, PartsUsed, DateComplete) VALUES (NULL, NULL, :UserID,  :Room, NULL, :Broken_Mouse,  :Broken_Keyboard, :Broken_Screen, :ExtraInfo, 1, NULL, NULL);";
+
+  $JobRequest_sql = "INSERT INTO  job
+    (JobID, DateLogged, UserID, RoomID, AssetID, Broken_Mouse, Broken_Keyboard, Broken_Screen, ExtraInfo, JobStatusID, PartsUsed, DateComplete) VALUES (NULL, CURRENT_TIMESTAMP, :UserID, :Room, NULL, :Broken_Mouse,  :Broken_Keyboard, :Broken_Screen, :ExtraInfo, 1, NULL, NULL);";
 
   $conn = dbConnect();
+  $stmt = $conn->prepare($JobRequest_sql);
   $stmt->bindParam(':UserID', $UserID);
   $stmt->bindParam(':Room', $Room);
   $stmt->bindParam(':Broken_Mouse', $Broken_Mouse);
@@ -18,7 +21,6 @@
   $stmt->bindParam(':Broken_Screen', $Broken_Screen);
   $stmt->bindParam(':ExtraInfo', $ExtraInfo);
 
-  $stmt = $conn->prepare($JobRequest_sql);
   $stmt->execute();
   header('location: /SMS/View/');
  ?>
